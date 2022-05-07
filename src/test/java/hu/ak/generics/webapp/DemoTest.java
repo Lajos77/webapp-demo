dc68da856b54f6d23c2c00a4597bbb1bb85782dc
xy <tozsa.lajos@gmail.com>
2022-05-07 11:15:51 +0200

    Sample test added

---
+++ src/test/java/hu/ak/generics/webapp/DemoTest.java
@@ -0,0 +1,21 @@
+package hu.ak.generics.webapp;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class DemoTest {
+	
+	@Test
+	public void testSucces() {
+		assertTrue(3 == 3);
+	}
+	
+	
+	@Test
+	@Disabled
+	public void testFailure() {
+		assertTrue(2 == 2);
+	}
+}
